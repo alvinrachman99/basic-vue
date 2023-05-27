803c1be95a717ca3641d9f1ef0e54565213de2af
Alvin Rachman Septian <alvinrachman16@gmail.com>
2023-05-28 00:42:36 +0700

    vue api test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    mounted(){
+      axios.get("https://b3d2-182-3-47-205.ngrok-free.app/api/unit")
+        .then(response => console.log(response.data))
+    }
+  }
+</script>
